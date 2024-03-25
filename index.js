0fcb10c77e02ea3c654fcee1028b56f4ee6f95f9
meghalakhwan <meghalakhwan9@gmail.com>
2024-03-25 23:08:16 +0530

    Project Completed

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const newYearTime = new Date("Jan 1, 2024 00:00:00").getTime();
 updateCountdown();
 
 function updateCountdown() {
-    const now = new Date().getTime();
-    const gap = newYearTime - now;
+  const now = new Date().getTime();
+  const gap = newYearTime - now;
 
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    const d = Math.floor(gap / day);
-    const h = Math.floor((gap % day) / hour);
-    const m = Math.floor((gap % hour) / minute);
-    const s = Math.floor((gap % minute) / second);
-    dayE1.innerText = d;
-    hourE1.innerText = h;
-    minuteE1.innerText = m;
-    secondE1.innerText = s;
-    setTimeout(updateCountdown, 1000)
+  const d = Math.floor(gap / day);
+  const h = Math.floor((gap % day) / hour);
+  const m = Math.floor((gap % hour) / minute);
+  const s = Math.floor((gap % minute) / second);
+  dayE1.innerText = d;
+  hourE1.innerText = h;
+  minuteE1.innerText = m;
+  secondE1.innerText = s;
+  setTimeout(updateCountdown, 1000);
 }
